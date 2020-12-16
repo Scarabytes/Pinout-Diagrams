0ff6f8ab4e137b069e5241286e8ffd6ab4df61ab
Julian Schmidt <39307101+Scarabytes@users.noreply.github.com>
2020-12-16 09:38:03 +0100

    Change rendering script to allow full filenames

--- Scripts/render.sh
+++ Scripts/render.sh
@@ -1,5 +1,12 @@
-#! /bin/sh
+#! /bin/bash
 
-inkscape -z -A "$1.pdf" "$1.svg"
-inkscape -z -e "$1.png" -b "#ffffff" "$1.svg"
-inkscape -z -e "$1 Large.png" -b "#ffffff" -d 200 "$1.svg"
+# Accepts any filename or path with .svg extension
+[[ $1 =~ ^(.*)(\.svg)$ ]]
+file_name=${BASH_REMATCH[1]}
+
+echo Rendering $file_name.svg
+echo
+
+inkscape -z -A "$file_name.pdf" "$file_name.svg"
+inkscape -z -e "$file_name.png" -b "#ffffff" "$file_name.svg"
+inkscape -z -e "$file_name Large.png" -b "#ffffff" -d 200 "$file_name.svg"
